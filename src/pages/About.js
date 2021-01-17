704b77ea13aa51e396e1541485bb763010811fdc
Mathieu Dessaint <mathieu.dessaint10@gmail.com>
2021-01-17 16:42:57 +0100

    responsive + img

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import photo from '../images/photo.jpg'
 
 export class About extends Component {
 
@@ -8,9 +7,10 @@ export class About extends Component {
             <div className="about section">
                 <h2>À propos de moi</h2>
                 <div className="about-me">
-                    <p>Passionné par le développement web, je me suis spécialisé en Javascript en utilisant principalement Vuejs et React côté frontend, et Nodejs/Express pour le backend.
-                    Je développe actuellement une application (réseau social) de pêche, qui sera déployé sur Docker et bientôt disponible sur le web.</p>
-                    <img src={photo} alt="profile"></img>
+                    <div className="wrapper-picture">
+                    </div>
+                    <p>“Passionné par le développement web, je me suis spécialisé en Javascript en utilisant principalement Vuejs et React côté frontend, et Nodejs/Express pour le backend.
+                    Je développe actuellement une application (réseau social) de pêche, qui sera déployé sur Docker et bientôt disponible sur le web.”</p>    
                 </div>       
             </div>
         )
