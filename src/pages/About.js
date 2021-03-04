509ef8066452eaa44ff72c306979b8de876cba1d
Mathieu Dessaint <mathieu.dessaint10@gmail.com>
2021-03-04 14:26:58 +0100

    about update

--- src/pages/About.js
+++ src/pages/About.js
@@ -12,13 +12,13 @@ export class About extends Component {
                     <p>Si je devais qualifier mon parcours en un mot, je dirais atypique.
                         En effet, après une licence en management international bilingue anglais et une expérience professionnelle en tant que chargé d’affaires, je me suis rendu compte que j’avais absolument besoin d’être épanoui dans mon travail. 
                         Me réorienter vers le développement web m’a donc paru évident étant donné que j’ai toujours été passionné par ce domaine.
-                        J'ai donc suivi une formation pour acquérir des connaissances solides. <br></br> <br></br> Autodidacte, je continue perpétuellement à élargir mes compétences, même si je me suis spécialisé en <span class="important-words">Javascript. </span>
-                        Les technologies  principales que j’utilise sont <span class="important-words">Vuejs et React côté frontend, Nodejs/Express en backend et Symfony récemment (PHP). </span>
-                        Ce que j’aime avant tout dans ce métier, c’est la  réalisation de projets et le challenge quotidien, ce qui est en adéquation avec ma personnalité. Persévérant et autonome, je n’abandonne jamais devant la difficulté. En effet, résoudre des problèmes ne me fait pas peur, bien au contraire !
-                        D’ailleurs, je développe actuellement une application de pêche (Fish & Follow) qui sera bientôt disponible sur le web.<br></br>
+                        J'ai donc suivi une formation pour acquérir des connaissances solides. <br></br> <br></br> Autodidacte, je continue d'élargir mes compétences, même si je me suis spécialisé en <span class="important-words">Javascript. </span>
+                        Les technologies  principales que j’utilise sont <span class="important-words">Vuejs</span> et <span class="important-words">React côté frontend, Nodejs/Express en backend</span> et <span class="important-words">Symfony/PHP récemment (projet Speedype). </span>
+                        Ce que j’aime dans ce métier, c’est la  réalisation de projets et le challenge quotidien, ce qui est en adéquation avec ma personnalité. Persévérant et autonome, je n’abandonne jamais devant la difficulté. En effet, résoudre des problèmes ne me fait pas peur.
+                        Je continue de développer l'application Fish & Follow, déjà accessible.<br></br>
                         <br></br>
-                        Ce que je recherche aujourd'hui, c’est acquérir davantage d’expérience professionnelle. 
-                        C’est pourquoi si vous avez diverses missions à me proposer ou un CDD / CDI, n'hésitez pas à me contacter, je me ferais un plaisir d’échanger avec vous.
+                        Ce que je recherche aujourd'hui, c’est acquérir de l'expérience professionnelle en tant que développeur. 
+                        C’est pourquoi si vous avez diverses missions à me proposer ou CDI/CDD, n'hésitez pas à me contacter, je me ferais un plaisir d’échanger avec vous.
                     </p>  
                 </div>       
             </div>
