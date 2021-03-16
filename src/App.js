30b07a273afb947ee959ec558df6d703eebdec56
Mathieu Dessaint <mathieu.dessaint10@gmail.com>
2021-03-16 11:26:59 +0100

    className

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css'
+import './styles/App.css'
 import MainWrapper from './components/MainWrapper'
 import "animate.css/animate.min.css"
 
--- src/pages/About.js
+++ src/pages/About.js
@@ -12,8 +12,8 @@ export class About extends Component {
                     <p>Si je devais qualifier mon parcours en un mot, je dirais atypique.
                         En effet, après une licence en management international bilingue anglais et une expérience professionnelle en tant que chargé d’affaires, je me suis rendu compte que j’avais absolument besoin d’être épanoui dans mon travail. 
                         Me réorienter vers le développement web m’a donc paru évident étant donné que j’ai toujours été passionné par ce domaine.
-                        J'ai donc suivi une formation pour acquérir des connaissances solides. <br></br> <br></br> Autodidacte, je continue d'élargir mes compétences, même si je me suis spécialisé en <span class="important-words">Javascript. </span>
-                        Les technologies  principales que j’utilise sont <span class="important-words">Vuejs</span> et <span class="important-words">React côté frontend, Nodejs/Express en backend</span> et <span class="important-words">Symfony/PHP récemment (projet Speedype). </span>
+                        J'ai donc suivi une formation pour acquérir des connaissances solides. <br></br> <br></br> Autodidacte, je continue d'élargir mes compétences, même si je me suis spécialisé en <span className="important-words">Javascript. </span>
+                        Les technologies  principales que j’utilise sont <span className="important-words">Vuejs</span> et <span className="important-words">React côté frontend, Nodejs/Express en backend</span> et <span className="important-words">Symfony/PHP récemment (projet Speedype). </span>
                         Ce que j’aime dans ce métier, c’est la  réalisation de projets et le challenge quotidien, ce qui est en adéquation avec ma personnalité. Persévérant et autonome, je n’abandonne jamais devant la difficulté. En effet, résoudre des problèmes ne me fait pas peur.
                         Je continue de développer l'application Fish & Follow, déjà accessible.<br></br>
                         <br></br>
